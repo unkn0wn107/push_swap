d9c3c9c9a8a0ffe04f73d5389fc9828d06d1b4a0
agaley <agaley@student.42lyon.fr>
2023-05-11 00:28:24 +0200

    Check for duplicates and invalid integers

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 01:41:58 by agaley            #+#    #+#             */
-/*   Updated: 2023/05/10 00:51:53 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/05/11 00:18:38 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,19 @@ int	main(int argc, char **argv)
 {
 	char	*str;
 	t_stack	*stack_a;
-	// t_stack	stack_b;
 
 	if (argc < 2)
 		exit_error(2);
 	str = ft_realloc(NULL, 1);
 	str = parse_args(str, argv);
-	ft_printf("%s\n", str);
 	stack_a = stack_init();
 	fill_stack(str, stack_a);
-	//check_duplicates(stack_a);
-	//select_algo(stack_a);
-	//stack_b = stack_init();
-	//apply_algo(stack_a, &stack_b);
-	//print_result(stack_a);
-	//free_stacks(stack_a, &stack_b);
 	exit (0);
 }
+
+// TODO
+// select_algo(stack_a);
+// stack_b = stack_init();
+// apply_algo(stack_a, stack_b);
+// print_result(stack_a);
+// free_stacks(stack_a, stack_b);
--- src/stack_adapter.c
+++ src/stack_adapter.c
@@ -6,45 +6,54 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 00:53:05 by agaley            #+#    #+#             */
-/*   Updated: 2023/05/10 21:35:38 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/05/11 00:27:20 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void	fill_stack(char *str, t_stack *stack_a)
-// {
-// 	int		i;
-// 	char	**arr;
-// 	t_node	*node;
-
-// 	i = 0;
-// 	arr = ft_split(str, ' ');
-// 	while (arr[i])
-// 	{
-// 		node = malloc(sizeof(t_node));
-// 		node->addr =
-// 		stack_add(stack_a, ft_atoi(arr[i++])); // ft_atoll to be implemented
-// 	}
-// }
-
+// TODO : ft_atoll to be implemented
 void	fill_stack(char *str, t_stack *stack_a)
 {
-	int		i;
-	int		num;
-	char	**arr;
+	size_t				i;
+	long long int		num;
+	char				**arr;
+	t_node				node;
 
-	(void)stack_a;
 	i = 0;
 	arr = ft_split(str, ' ');
 	while (arr[i])
+		i++;
+	while (i--)
 	{
 		num = ft_atoi(arr[i]);
-		printf("%d\n", INT_MIN);
-		// num < INT_MIN || num > INT_MAX : how to check
-		if (num == 0 && !ft_strchr(arr[i], '0'))
-			exit_error(4);
-		ft_printf("%d\n", num); // ft_atoll to be implemented
-		i++;
+		if ((num == 0 && !ft_strchr(arr[i], '0'))
+			|| ft_strncmp(ft_itoa(num), arr[i], ft_strlen(arr[i])) != 0)
+			exit_error(22);
+		if (stack_get_node(stack_a, num))
+			exit_error(21);
+		node.value = num;
+		stack_add(stack_a, node);
 	}
+}
+
+/**
+ * Return node with value in a stack
+ *
+ * @param stack pointer towards stack anchor
+ * @param value value of node
+ * @return first node in stack
+ */
+t_node	*stack_get_node(t_stack *stack, long long int value)
+{
+	t_node	*node;
+
+	node = stack->head;
+	while (node)
+	{
+		if (node->value == value)
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
 }
