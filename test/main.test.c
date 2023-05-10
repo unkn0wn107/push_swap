42aebcb8418f49157e18ddd0a7e6e8c447db3de2
agaley <agaley@student.42lyon.fr>
2023-05-11 00:37:35 +0200

    Test return 1 on error

--- test/main.test.c
+++ test/main.test.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 00:25:08 by agaley            #+#    #+#             */
-/*   Updated: 2023/05/11 00:04:01 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/05/11 00:37:03 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 int	main(void)
 {
-	test_stack_free();
-	test_stack_swap();
-	test_stack_push();
-	test_stack_rotate();
-	test_stack_rrotate();
-	return (0);
+	int	ret;
+
+	ret = 0;
+	if (test_stack_free() != 0)
+		ret = 1;
+	if (test_stack_swap() != 0)
+		ret = 1;
+	if (test_stack_push() != 0)
+		ret = 1;
+	if (test_stack_rotate() != 0)
+		ret = 1;
+	if (test_stack_rrotate() != 0)
+		ret = 1;
+	return (ret);
 }
